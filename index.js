28fa789d653781d337b45705e4676920f581055d
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-29 16:35:48 -0400

    added mongose db functionality

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
 =================
 Building Web Apps
-October 22-2016
+October 22,29-2016
 Class by Abhishek Singh
 Quiz App
 =================
 */
 
 /* Require Modules */
-var express = require('express'); // Express knows where is pug
+var express = require('express'); // Express knows where is pug, so we dont need to import pug
 var http = require('http');
 var path = require('path'); // Helps finds path to things
 
@@ -17,6 +17,30 @@ var app = express(); //Create Express App. Take the variable express and create 
 http = http.Server(app); // Create server with express inside it
 const BASE = '/';
 
+/* Database with Mongoose */
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://admin:6424.uai@ds045628.mlab.com:45628/itp-dbexample"); // Connnect to DB
+var db = mongoose.connection; // Check for working connection. Only after mongoose.connect
+
+/* Callback for Database*/
+db.on('open', function(){ // When the db variables has a 'open' will call the funciton
+  console.log("I have connected to the database.");
+})
+
+/* Create a Schema (definition of what is inside the fields in the db) */
+var Schema = mongoose.Schema; // Import the Schema Object from Mongoose Module
+var userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true   // dont save the white spaces if someone writes something like "    juan      "
+  },
+  email:{
+    type: String,
+    trim: true
+  }
+})
+
+var User = mongoose.model('User', userSchema); // Create a user model based on the userSchema
 
 /*== Middleware, require before responds in sent (in between req and res) ==*/
 /*Use Pug for HTML Templeting*/
@@ -28,7 +52,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-/*== Client call ===*/
+
+
+/* ==== Client Calls ==== */
+
+
 /* Main Route Function when going to localhost:3000 */
 app.get(BASE, function(req, res){ // The browser is goint to send a 'req' to BASE and server sends 'res'
   //res.send("<h1>Hello ITP</h1>"); // Send plain text
@@ -38,7 +66,6 @@ app.get(BASE, function(req, res){ // The browser is goint to send a 'req' to BAS
   // add first name and last name
   // set it to a variable
   // pass that variable to the template
-
   res.render("index.pug", {myname:name}) // Render index pug file and pass the variable myname
 })
 
@@ -56,10 +83,57 @@ app.get(BASE+'about', function (req,res){
 app.get(BASE+'contact', function (req,res){
   //res.send("Contact");
 })
-/*== Client call ===*/
 
 
+/*--- Database based Calls  ---*/
 
+/* First: Route for the Database that Adds a new user everytime we acces /user */
+app.get(BASE+'user', function(req,res){
+
+  var newUser = new User({
+    name: "juan"    // Pass a value according to the schema we defined
+  })
+
+  newUser.save(function(err, result){ // When the new user gets saved run this function. This will create a new user everytime we access /user
+
+    if(err){
+      console.log(err);
+      res.send("Error saving your name");
+    }else{
+      res.send("Saved to database");
+    }
+  })
+
+})
+
+/* Second: Show all saved users in the database */
+app.get(BASE+'all', function(req,res){
+  User.find({}, function(err, results){ // Within the "User Schema" find everything
+
+    if(err){
+      res.send("cannot find everything");
+    }
+
+    return res.json(
+      {
+      data: results
+      }
+    )
+  })
+})
+
+/* Third: Query the database: only shows something specific. In this case "cristobal" */
+app.get(BASE+'filter', function(req, res){
+  User.find({name: "cristobal"}, function(err, result){
+    return res.json(
+      {
+        data: result
+      }
+    )
+  })
+})
+
+/*== Client Calls ===*/
 
 
 
